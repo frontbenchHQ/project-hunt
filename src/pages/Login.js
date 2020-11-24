8389eed669eed11711d4d61b5f2ca7fdc385472e
rutik wankhade <rutikwankhade2@gmail.com>
2020-11-24 16:49:36 +0530

    add layout component and pages

---
+++ src/pages/Login.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Login = () => {
+    return ( 
+        <h2>This is a login page</h2>
+     );
+}
+ 
+export default Login;
---
+++ src/pages/ProjectShowcase.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const ProjectShowcase = () => {
+    return ( 
+        <h2>This is a project showcase page</h2>
+     );
+}
+ 
+export default ProjectShowcase;
