dc37f4aed8abb82867186eac0a85ea61fbd65eb6
rutik wankhade <rutikwankhade2@gmail.com>
2020-11-26 16:45:14 +0530

    add intro and illustration

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import FeaturedProjects from '../components/FeaturedProjects';
+import collabImage from '../assets/images/collab-image.svg'
 const HomePage = () => {
     return (
-        <div className="mt-20 text-center">
+        <div className=" text-center">
+            <div className="flex md:flex-row flex-col-reverse ">
+                <div className="text-gray-700 md:mt-36 md:ml-28 mt-6 m-5 md:text-left text-center">
+                    <p className="text-lg">A community of makers</p>
+                    <h1 className="md:text-4xl text-3xl font-bold ">Build projects and share with community</h1>
+                    <button className="bg-purple-500 text-white px-4 py-2 rounded text-xl mt-4">Join Now</button>
+                </div>
+
+                <img src={collabImage} className="md:m-4 md:max-w-screen-md" />
+            </div>
             <div className="max-w-screen-md ">
-                <FeaturedProjects/>
+                <FeaturedProjects />
             </div>
         </div>
     );
