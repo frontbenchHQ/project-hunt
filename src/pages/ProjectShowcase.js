5f8618f777af217c23808d57c10b0e1a65d308cd
rutik wankhade <rutikwankhade2@gmail.com>
2020-12-07 15:11:31 +0530

    feat: add dropdown component

--- src/pages/ProjectShowcase.js
+++ src/pages/ProjectShowcase.js
@@ -1,9 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
+import allProjects from 'data/projectsData';
+import ProjectCard from '../components/ProjectCard';
+import Dropdown from '../components/Dropdown';
 
 const ProjectShowcase = () => {
+    const options = ['Top Voted', 'Recently added', 'Featured']
+
+    const [selected, setSelected] = useState(options[0]);
+
     return (
-        <div className="m-20 text-center">
-            <h2>Here are some awesome projects</h2>
+        <div className="md:px-60 p-2 text-center">
+            <h2 className="text-3xl font-bold text-gray-600 font-sans m-8">Showcase</h2>
+
+            <Dropdown
+                options={options}
+                selected={selected}
+                onSelectedChange={setSelected}
+            />
+            <div>
+                {
+                    allProjects.map(project => {
+                        return <ProjectCard project={project} />
+
+                    })
+                }
+            </div>
         </div>
     );
 }
